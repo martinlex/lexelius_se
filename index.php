9d8b48162ebed1f0fa91534e6c5bd90fea024807
martinlex <info@lexelius.se>
2016-01-30 22:49:54 +0100

    SVG
    Replaced Font Awesome icons with optimized svg icons
    Added new icon for Myfonts
    First prototype for single post view

--- index.php
+++ index.php
@@ -10,18 +10,26 @@
 		<?php require 'include/header.html' ?>
 		<?php require 'include/contact.html' ?>
 
+		<!-- Single post -->
+		<div ng-show="thePost">
+			<div ng-repeat="result in results | filter:clickedPost">
+			{{result.id}}, {{result.title}}, {{result.body}}
+			<span ng-click="hidePost();">[x]</span>
+			</div>
+		</div>
+
+		<!-- All posts -->
 		<div class="main-content">
 			<div ng-repeat="post in posts | filter:filters">
-				<div id="{{post.id}}" class="list-item full {{post.category}}" ng-class-odd="'odd'" ng-class-even="'even'">
-					<div class="post-image thumbnail quarter left" style="background-image: url('thumb/{{post.thumbnail_img}}')">
+				<div class="list-item full {{post.category}}" ng-class-odd="'odd'" ng-class-even="'even'">
+					<div class="post-image thumbnail quarter left" style="background-image: url('thumb/{{post.thumbnail_img}}')" data="{{post.id}}" ng-click="showPost(post.id)">
 					</div>
 					<div class="post-title ellipsified left">
-						<h1>{{post.title}}</h1>
+						<h1 data="{{post.id}}" ng-click="showPost(post.id);">{{post.title}}</h1>
 					</div>
 				</div>
 			</div>
-
-		</div> <!-- End main-content -->
+		</div>
 
 	</div> <!-- End wrapper -->
 
--- js/controller.js
+++ js/controller.js
@@ -3,20 +3,32 @@ app.controller('mainCtrl', function($scope, $http) {
 	// Load posts from mySQL and JSON
 	$http.get('./include/query_posts.php').then(function (response) {
 		$scope.posts = response.data.records;
+		// Make duplicate for single post
+		$scope.results = $scope.posts;
 	});
-	// Filtering posts in nav
+	// Filtering posts in menu
 	$scope.filters = { };
 	$scope.categories = [
 		{name: 'Web Dev', id: 'cat_dev'},
 		{name: 'Typsnitt', id: 'cat_type'},
 		{name: 'Målningar', id: 'cat_arts'}
 	];
-	// Show and hide
-	$scope.showMe = function() {
-		$scope.show = true;
+	// Show and hide contact
+	$scope.showAbout = function() { $scope.showContact = true;  }
+	$scope.hideAbout = function() { $scope.showContact = false; }
+
+	// Show single post
+	$scope.showPost = function(id) {
+		console.log(id);
+		// Change filter value in results repeat
+		$scope.clickedPost = function(results) {
+			return results.id === id;
+		}
+		// Show the post
+		$scope.thePost = true; 	// Show the post
 	}
-	$scope.hideMe = function() {
-		$scope.show = false;
-	}
+
+	// Close single post
+	$scope.hidePost = function() { $scope.thePost = false; }
 
 });
